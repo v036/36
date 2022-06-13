826bdc642f5f9b8c83d8559dfb0f2d264fb7a2f0
v036 <102698287+v036@users.noreply.github.com>
2022-06-13 23:20:06 +0200

    Update index.js

--- api/device/index.js
+++ api/device/index.js
@@ -1,13 +1,1 @@
-const fpPromise = import('https://fpcdn.io/v3/nDQd96V5VSW0V679K3o8')
-.then(FingerprintJS => FingerprintJS.load());
-
-// Get the visitor identifier when you need it.
-fpPromise
-.then(fp => fp.get())
-//.then(result => $('#response').html(JSON.stringify(result)));
-//.then(result => console.log(result.visitorId));
-const express = require('express');
-
-const app = express();
-
-app.use(express.json(result)); //This is the line that you want to add
+console.log("uwu roblox")
